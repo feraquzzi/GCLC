0716aa6d449acd41b28c3944d52569ad263982bb
feraquzzi <mosesobembe@gmail.com>
2024-04-17 18:45:45 +0100

    added extra student data

---
+++ assets/js/togglepassword.js
@@ -0,0 +1,23 @@
+const togglePassword = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+togglePassword.addEventListener("click", function() {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    
+    // toggle the icon
+    this.classList.toggle("bi-eye");
+});
+
+/*const togglePassword1 = document.querySelector("#togglePassword1");
+const password1 = document.querySelector("#inputPassword");
+
+togglePassword1.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password1.getAttribute("type") === "password" ? "text" : "password";
+    password1.setAttribute("type", type);
+    
+    // toggle the icon
+    this.classList.toggle("bi-eye");
+});*/
